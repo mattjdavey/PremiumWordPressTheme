3067ccd5d0224fbf553ad559b692a34b302e8b06
Matt Davey <matt.davey12@gmail.com>
2018-09-19 16:18:14 -0500

    Added more settings fields

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -13,11 +13,33 @@ function premium_add_admin_page() {
 
     // Generate premium admin sub pages.
     add_submenu_page('premium', 'Premium Theme Options', 'General', 'manage_options', 'premium', 'premium_theme_create_page');
-
     add_submenu_page('premium', 'Premium CSS Options', 'Custom CSS', 'manage_options', 'premium_css', 'premium_theme_css_page');
+
+    // Activate custom settings.
+    add_action( 'admin_init', 'premium_custom_settings' );
 
 }
 add_action( 'admin_menu', 'premium_add_admin_page' );
+
+function premium_custom_settings() {
+    register_setting( 'premium-settings-group', 'first_name' );
+    register_setting( 'premium-settings-group', 'last_name' );
+
+    add_settings_section('premium-sidebar-options', 'Sidebar Options', 'premium_sidebar_options', 'premium');
+
+    add_settings_field('sidebar-name', 'Full Name', 'premium_sidebar_name', 'premium', 'premium-sidebar-options');
+}
+
+function premium_sidebar_options() {
+    echo 'Customize Your Sidebar Information';
+}
+
+function premium_sidebar_name() {
+    $firstName = esc_attr(get_option( 'first_name' ));
+    $lastName = esc_attr(get_option( 'last_name' ));
+    echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name" />' .
+    '<input type="text" name="last_name" value="'.$lastName.'" placeholder="Last Name" />';
+}
 
 function premium_theme_create_page() {
     
--- inc/templates/premium-admin.php
+++ inc/templates/premium-admin.php
@@ -1,4 +1,9 @@
 <h1>Premium Theme Options</h1>
-<h3 class="title">Manage Options</h3>
-<p>Customize the default WordPress Options</p>
-<?php bloginfo('name'); ?>
+<?php settings_errors(); ?>
+<form action="options.php" method="post">
+
+    <?php settings_fields('premium-settings-group'); ?>
+    <?php do_settings_sections('premium'); ?>
+    <?php submit_button(); ?>
+
+</form>
