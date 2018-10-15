c49da59854133fd1c202c46abeb7c4c7ad6fea38
Matt Davey <mdavey@clearwater.mn.us>
2018-10-15 16:20:07 -0500

    Added custom meta box for email address.

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -16,6 +16,9 @@ if ( @$contact == 1 ) {
 
     add_filter( 'manage_premium-contact_posts_columns', 'premium_set_contact_columns' );
     add_action( 'manage_premium-contact_posts_custom_column', 'premium_contact_custom_column', 10, 2 );
+
+    add_action( 'add_meta_boxes', 'premium_contact_add_meta_box');
+    add_action( 'save_post', 'premium_save_contact_email_data' );
 }
 
 /* CONTACT CPT */
@@ -61,9 +64,54 @@ function premium_contact_custom_column( $column, $post_id ) {
             echo get_the_excerpt();
             break;
         case 'email' :
-            echo 'email address';
+            echo get_post_meta( $post_id, '_contact_email_value_key', true);            
             break;
 
     }
+
+}
+
+/* CONTACT META BOXES */
+
+function premium_contact_add_meta_box() {
+    add_meta_box( 'contact_email', 'User Email', 'premium_contact_email_callback', 'premium-contact', 'side', 'high' );
+}
+
+function premium_contact_email_callback( $post ) {
+    wp_nonce_field( 'premium_save_contact_email_data', 'premium_contact_email_meta_box_nonce' );
+
+    $value = get_post_meta( $post->ID, '_contact_email_value_key', true);
+
+    echo '<label for="premium_contact_email_field">User Email Address: </label>';
+    echo '<input type="email" id="premium_contact_email_field" name="premium_contact_email_field" value="' . esc_attr( $value ) . '" size="25" />';
+
+} 
+
+function premium_save_contact_email_data( $post_id ) {
+
+    if( ! isset($_POST['premium_contact_email_meta_box_nonce'] )) {
+        return;
+    }
+
+    if ( ! wp_verify_nonce( $_POST['premium_contact_email_meta_box_nonce'], 'premium_save_contact_email_data')) {
+        return;
+    }
+
+    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
+        return;
+    }
+
+    if ( ! current_user_can( 'edit_post', $post_id) ) {
+        return;
+    }
+
+    if ( ! isset( $_POST['premium_contact_email_field'] ) ) {
+        return;
+    }
+
+    $my_data = sanitize_text_field( $_POST['premium_contact_email_field'] );
+
+    update_post_meta( $post_id, '_contact_email_value_key', $my_data );
+
 
 }
