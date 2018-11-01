f803c6ba3071dc5ee952893587e038c4a79f19bf
Matt Davey <mdavey@clearwater.mn.us>
2018-11-01 13:41:35 -0500

    Added navigation. Added standard post type template.

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -19,7 +19,7 @@ function premium_load_admin_scripts( $hook ) {
         wp_enqueue_script( 'premium-admin-script' );
     } else if ( 'premium_page_premium_css' == $hook ) {
 
-        wp_enqueue_style('ace-css', get_template_directory_uri() . '/css/sunset.ace.css', array(), '1.0.0', 'all' );
+        wp_enqueue_style('ace-css', get_template_directory_uri() . '/css/premium.ace.css', array(), '1.0.0', 'all' );
 
         wp_enqueue_script( 'ace', get_template_directory_uri() . '/js/ace/ace.js', array('jquery'), '1.2.1', true );
         wp_enqueue_script( 'premium-custom-css-script', get_template_directory_uri() . '/js/premium.custom_css.js', array('jquery'), '1.0.0', true );
@@ -28,3 +28,24 @@ function premium_load_admin_scripts( $hook ) {
 
 }
 add_action( 'admin_enqueue_scripts', 'premium_load_admin_scripts' );
+
+/*
+    ====================================
+        FRONT-END ENQUEUE FUNCTIONS
+    ====================================
+*/
+
+function premium_load_scripts() {
+
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.6', 'all' );
+    wp_enqueue_style( 'premium', get_template_directory_uri() . '/css/premium.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'raleway', 'https://fonts.googleapis.com/css?family=Raleway:200,300,500' );
+    wp_enqueue_style( 'fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' );
+
+    wp_deregister_script( 'jquery' );
+    wp_register_script( 'jquery', get_template_directory_uri() . '/js/jquery.js', false, '1.11.3', true );
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.6', true );
+
+}
+add_action('wp_enqueue_scripts', 'premium_load_scripts' );
--- inc/theme-support.php
+++ inc/theme-support.php
@@ -3,9 +3,9 @@
 /*
 
 @package premiumtheme
- ====================================
+    ====================================
         THEME SUPPORT OPTIONS
-====================================
+    ====================================
 
 */
 
@@ -27,4 +27,58 @@ if ( @$header == 1 ) {
 $background = get_option( 'custom_background' );
 if ( @$background == 1 ) {
     add_theme_support('custom-background');
+}
+
+add_theme_support( 'post-thumbnails' );
+
+/* Activate Nav Menu Option */
+function premium_register_nav_menu() {
+    register_nav_menu( 'primary', 'Header Navigation Menu');
+}
+add_action( 'after_setup_theme', 'premium_register_nav_menu' );
+
+/* 
+    ====================================
+        BLOG LOOP CUSTOM FUNCTION
+    ====================================
+*/
+function premium_posted_meta() {
+    
+    $posted_on = human_time_diff( get_the_time('U'), current_time( 'timestamp') );
+    
+    $categories = get_the_category();
+    $separator = ', ';
+    $output = '';
+    $i = 1;
+
+    if ( !empty($categories) ):
+    
+        foreach( $categories as $category ):
+            if ( $i > 1 ): $output .= $separator; endif; 
+
+            $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( 'View all posts in%s', $category->name) . '">' . esc_html( $category->name) . '</a>';
+            $i++;
+        endforeach;
+    endif;
+    
+    return '<span class="posted-on">Posted <a href="' . esc_url( get_the_permalink() ) . '">' . $posted_on . ' ago</span> / <span class="posted-in">' . $output . '</span>';
+}
+
+function premium_posted_footer() {
+
+    $comments_num = get_comments_number();
+    if ( comments_open() ) {
+        if( $comments_num == 0 ) {
+            $comments = __('No comments');            
+        } elseif ( $comments_num > 1 ) {
+            $comments = $comments_num . __(' Comments');
+        } else {
+            $comments = __('1 Comment');
+        }
+        $comments = '<a href="'. get_comments_link() .'">' . $comments . ' <span class="fa fa-comments"></span></a>';
+    } else {
+        $comments = __('Comments are closed');
+    }
+
+    return '<div class="post-footer-container"><div class="row"><div class="col-xs-12 col-sm-6">'. get_the_tag_list( '<div class="tags-list"><span class="fa fa-tags"></span> ', ' ', '</div>') .'</div><div class="col-xs-12 col-sm-6">'. $comments .'</div></div></div>';
 }
